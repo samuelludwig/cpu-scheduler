cf9c1ed4693fa4e961534aa9be8d71bb8b98222b
Samuel Ludwig <SLudwig.dev@gmail.com>
2019-10-22 08:47:11 -0400

    Refactor Round-Robin code

--- cpuscheduler/lib/rr.ex
+++ cpuscheduler/lib/rr.ex
@@ -1,30 +1,21 @@
 defmodule Rr do
-  import Util
-
-  # This module will have it's own version of the build_process_time_datum_struct_from_queue and build_gantt_datum_struct_from_queue functions as the algorithm is not as simple as simply sorting the processes in a certain way.
+  # The implementation for the Round Robin algorithm is not as generic as the
+  # implementation of the others, as it involves more than simply putting
+  # processes in the right order.
 
   def generate_process_times_list(process_queue, quantum) do
     generate_gantt_data_list(process_queue, quantum)
-    |> Enum.group_by(fn %GanttDatum{p_name: p_name} -> p_name end)
-    |> Enum.map(fn {p_name, process_gantt_list} ->
-      derive_wait_time_and_turnaround_time_from_map_of_gantt_data(p_name, process_gantt_list)
-    end)
+    |> condense_gantt_data_into_process_time_data()
   end
 
-  defp derive_wait_time_and_turnaround_time_from_map_of_gantt_data(p_name, gantt_list) do
-    wait_time =
-      Enum.min_by(gantt_list, fn %GanttDatum{start_time: start_time} ->
-        start_time
-      end).start_time
-
-    turnaround_time =
-      Enum.max_by(gantt_list, fn %GanttDatum{stop_time: stop_time} ->
-        stop_time
-      end).stop_time
-
-    %ProcessTimeDatum{p_name: p_name, wait_time: wait_time, turnaround_time: turnaround_time}
-  end
-
+  # We use a recursive solution for generating the gantt data list as we are
+  # operating on two separate datastrucures whose contents are dependant on one
+  # another on each iteration. We are working in a queue and each item needs to
+  # be evaluated separately. Therefore, iteration is the correct approach.
+  # We iterate over the process queue and move and remove things as needed,
+  # building the gantt data list with each step, we do this until there are no
+  # processes remaining in the queue. When there are no processes remaining in
+  # the queue, we return the completed gantt_data_list datastructure.
   def generate_gantt_data_list(gantt_data_list \\ [], process_queue, quantum)
 
   def generate_gantt_data_list(gantt_data_list, [], _quantum) do
@@ -32,19 +23,21 @@ defmodule Rr do
   end
 
   def generate_gantt_data_list(gantt_data_list, process_queue, quantum) do
-    start = get_stop_time_of_last_gantt_block(gantt_data_list)
-    [process] = Enum.take(process_queue, 1)
-
     {new_gantt_data_list, new_process_list} =
-      calculate_next_gantt_data_list_cycle(
-        gantt_data_list,
-        process_queue,
-        process,
-        start,
-        quantum
-      )
+      calculate_next_gantt_data_list_cycle(gantt_data_list, process_queue, quantum)
 
     generate_gantt_data_list(new_gantt_data_list, new_process_list, quantum)
+  end
+
+  defp calculate_next_gantt_data_list_cycle(gantt_data_list, process_list, quantum) do
+    start_time = get_stop_time_of_last_gantt_block(gantt_data_list)
+    process = List.first(process_list)
+
+    new_gantt_data_list = gantt_data_list ++ [calculate_gantt_block(process, start_time, quantum)]
+
+    new_process_list = calculate_cycled_list(process_list, process, quantum)
+
+    {new_gantt_data_list, new_process_list}
   end
 
   defp calculate_gantt_block(%CpuProcess{burst_size: burst_size} = process, start_time, quantum)
@@ -64,18 +57,6 @@ defmodule Rr do
     }
   end
 
-  defp calculate_next_gantt_data_list_cycle(
-         gantt_data_list,
-         process_list,
-         process,
-         start_time,
-         quantum
-       ) do
-    new_gantt_data_list = gantt_data_list ++ [calculate_gantt_block(process, start_time, quantum)]
-    new_process_list = calculate_cycled_list(process_list, process, quantum)
-    {new_gantt_data_list, new_process_list}
-  end
-
   defp calculate_cycled_list(process_list, %CpuProcess{burst_size: burst_size} = process, quantum)
        when burst_size > quantum do
     Enum.drop(process_list, 1) ++ [%{process | burst_size: process.burst_size - quantum}]
@@ -90,5 +71,19 @@ defmodule Rr do
   defp get_stop_time_of_last_gantt_block(gantt_data_list) do
     last_block = List.last(gantt_data_list)
     last_block.stop_time
+  end
+
+  defp derive_wait_time_and_turnaround_time_from_map_of_gantt_data(p_name, gantt_list) do
+    wait_time = List.first(gantt_list).start_time
+    turnaround_time = List.last(gantt_list).stop_time
+    %ProcessTimeDatum{p_name: p_name, wait_time: wait_time, turnaround_time: turnaround_time}
+  end
+
+  defp condense_gantt_data_into_process_time_data(gantt_data) do
+    gantt_data
+    |> Enum.group_by(fn %GanttDatum{p_name: p_name} -> p_name end)
+    |> Enum.map(fn {p_name, process_gantt_list} ->
+      derive_wait_time_and_turnaround_time_from_map_of_gantt_data(p_name, process_gantt_list)
+    end)
   end
 end
