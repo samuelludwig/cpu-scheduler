9a5355ec2e0862d6a2b1ba319135e350c67bf5be
Samuel Ludwig <SLudwig.dev@gmail.com>
2019-10-28 22:53:28 -0400

    Init mix project in charge of handling input and output

---
+++ cpuscheduler_adapter/lib/cpuscheduler_adapter.ex
@@ -0,0 +1,18 @@
+defmodule CpuschedulerAdapter do
+  @moduledoc """
+  Documentation for CpuschedulerAdapter.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CpuschedulerAdapter.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ cpuscheduler_adapter/test/cpuscheduler_adapter_test.exs
@@ -0,0 +1,8 @@
+defmodule CpuschedulerAdapterTest do
+  use ExUnit.Case
+  doctest CpuschedulerAdapter
+
+  test "greets the world" do
+    assert CpuschedulerAdapter.hello() == :world
+  end
+end
